c81a932b3027a6d1006576795dd22e25a441ba09
Jesse Tilley <jesseandrewtilley@gmail.com>
2017-08-24 23:16:28 +1000

    Add timer and ticker

--- program.cpp
+++ program.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <string>
 #include <ctime>
 
-//Can use difftime to calculate differnce between time.
+using namespace std;
+
 
 time_t get_current_time()
 {
@@ -10,12 +12,18 @@ time_t get_current_time()
     return current_time;
 }
 
+void ticker(time_t start_time, time_t current_time)
+{
+    double seconds;
+    seconds = std::difftime(current_time,start_time);
+    cout.precision(3);
+    {
+        cout << fixed << seconds << "\r";
 
-//void ticker(timestamp &current_time)
-//{
+    }
 
-//}
 
+}
 
 void print_time(time_t current_time)
 {   
@@ -23,14 +31,19 @@ void print_time(time_t current_time)
     local_time = localtime( &current_time );
     local_time->tm_mon  = local_time->tm_mon + 1;
     local_time->tm_year   = local_time->tm_year + 1900;
-    std::cout << "This program was exectued at: " << local_time->tm_hour << ":" << local_time->tm_min << ":" << local_time->tm_sec << std::endl;
+    std::cout << "Timer started at: " << local_time->tm_hour << ":" << local_time->tm_min << ":" << local_time->tm_sec << std::endl;
     std::cout << "And the current date is: " << local_time->tm_mday << "/" << local_time->tm_mon << "/" << local_time->tm_year << std::endl;   
 }
 
 int main()
 {
-    time_t current_time;
-    current_time = get_current_time();
+    time_t start_time, current_time;
+    start_time = get_current_time();
+    while(true)
+    {
+        current_time = get_current_time();
+        ticker(start_time, current_time);
+    }
     print_time(current_time);
 
     
