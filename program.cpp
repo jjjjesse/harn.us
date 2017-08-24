203e95134c416d1c9b1cb9ddd26cd86769a5d6ce
Jesse Tilley <jesseandrewtilley@gmail.com>
2017-08-24 21:37:31 +1000

    Simplified time types

--- program.cpp
+++ program.cpp
@@ -1,45 +1,38 @@
 #include <iostream>
 #include <ctime>
 
-struct timestamp
+//Can use difftime to calculate differnce between time.
+
+time_t get_current_time()
 {
-    int day;
-    int month;
-    int year;
-    int hour;
-    int min;
-    int sec;
-};
-
-struct tm *local_time;
-
-timestamp get_current_time()
-{
-    
-    timestamp result;
-
     time_t current_time;
-    time( &current_time );                   // Get the current time
-    local_time = localtime( &current_time );  // Convert the current time to the local time
+    time( &current_time );
+    return current_time;
+}
 
-    result.day    = local_time->tm_mday;
-    result.month  = local_time->tm_mon + 1;
-    result.year   = local_time->tm_year + 1900;
-    result.hour   = local_time->tm_hour;
-    result.min    = local_time->tm_min;
-    result.sec    = local_time->tm_sec;
 
-    return result;
+//void ticker(timestamp &current_time)
+//{
+
+//}
+
+
+void print_time(time_t current_time)
+{   
+    struct tm *local_time;
+    local_time = localtime( &current_time );
+    local_time->tm_mon  = local_time->tm_mon + 1;
+    local_time->tm_year   = local_time->tm_year + 1900;
+    std::cout << "This program was exectued at: " << local_time->tm_hour << ":" << local_time->tm_min << ":" << local_time->tm_sec << std::endl;
+    std::cout << "And the current date is: " << local_time->tm_mday << "/" << local_time->tm_mon << "/" << local_time->tm_year << std::endl;   
 }
 
 int main()
 {
-    timestamp current_time;
+    time_t current_time;
     current_time = get_current_time();
+    print_time(current_time);
 
-  //  std::cout << "This is currenttime: " << current_time << std::endl;
-  //  std::cout << "This is localtime: " << local_time << std::endl;
-    std::cout << "This program was exectued at: " << current_time.hour << ":" << current_time.min << ":" << current_time.sec << std::endl;
-    std::cout << "And the current date is: " << current_time.day << "/" << current_time.month << "/" << current_time.year << std::endl;
+    
     return 0;
 }
