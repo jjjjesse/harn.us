8127c9eb6242544dee7c7482c8539d22fdea1539
Jesse Tilley <jesseandrewtilley@gmail.com>
2017-08-25 11:29:37 +1000

    Add clear screen and fix text output

--- program.cpp
+++ program.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+ 
 
 using namespace std;
 
+void clear_screen()
+{
+   cout << string( 100, '\n' );
+}
 
 time_t get_current_time()
 {
@@ -16,9 +21,9 @@ void ticker(time_t start_time, time_t current_time)
 {
     double seconds;
     seconds = std::difftime(current_time,start_time);
-    cout.precision(3);
+    cout.precision(0);
     {
-        cout << fixed << seconds << "\r";
+        cout << "Time elapsed: " << fixed << seconds << "\r";
 
     }
 
@@ -31,14 +36,16 @@ void print_time(time_t current_time)
     local_time = localtime( &current_time );
     local_time->tm_mon  = local_time->tm_mon + 1;
     local_time->tm_year   = local_time->tm_year + 1900;
-    std::cout << "Timer started at: " << local_time->tm_hour << ":" << local_time->tm_min << ":" << local_time->tm_sec << std::endl;
+    std::cout << "Timer started at: " << local_time->tm_hour << ":" << local_time->tm_min << std::endl;
     std::cout << "And the current date is: " << local_time->tm_mday << "/" << local_time->tm_mon << "/" << local_time->tm_year << std::endl;   
 }
 
 int main()
 {
+    clear_screen();
     time_t start_time, current_time;
     start_time = get_current_time();
+    print_time(start_time);
     while(true)
     {
         current_time = get_current_time();
